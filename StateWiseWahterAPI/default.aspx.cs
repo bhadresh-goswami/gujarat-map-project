3a3382b957b7b0456149d5536b6a2ab63df97a81
bhadresh-goswami <gosai09@gmail.com>
2020-05-19 20:34:06 +0530

    upload all

---
+++ StateWiseWahterAPI/default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StateWiseWahterAPI
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Request.QueryString["id"]!=null)
+            {
+                int id = int.Parse(Request.QueryString["id"].ToString());
+                //here we can execute the back end code to get details from database
+            }
+        }
+    }
+}
